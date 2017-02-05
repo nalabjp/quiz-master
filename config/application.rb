1ac7349102f7ebf673c9709e50e7236d5544b05c
nalabjp <nalabjp@gmail.com>
2017-02-06 01:27:22 +0900

    Add configuration of factory_girl

--- config/application.rb
+++ config/application.rb
@@ -24,11 +24,13 @@ module QuizMaster
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
+        fixtures: true,
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
         controller_specs: false
+      g.fixture_replacement :factory_girl,
+        dir: "spec/factories"
     end
   end
 end
