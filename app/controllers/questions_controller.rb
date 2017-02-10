012adbdaa2a06f0ec08e148649bb89bf59e95a8b
nalabjp <nalabjp@gmail.com>
2017-02-10 21:59:07 +0900

    Work minimally

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -69,6 +69,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.fetch(:question, {})
+      params.require(:question).permit(:content, :answer)
     end
 end
